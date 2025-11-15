d10cbc6a4c1eab07aa68311613ca9b06eb2056c2
Arin Kathapurkar <arin.kathapurkar@duke.edu>
2025-11-15 17:47:37 -0500

    built scraper

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,16 +1,33 @@
+import ssl, certifi, urllib.request
+import os
 import pandas as pd 
 import Bio 
-from bio import Entrez
-from bio import SeqIO
+from Bio import Entrez
+from Bio import SeqIO
 import time
 
 Entrez.email = "arin.kathapurkar26@gmail.com"
+Entrez.api_key = os.getenv("NCBI_API_KEY")
+
+def normalize_header(df):
+    df.columns = (
+        df.columns.astype(str)
+        .str.replace(r"[\u00A0\u200B\u200E\u200F]", "", regex=True)  # remove invisible spaces
+        .str.replace(r"\s+", " ", regex=True)                       # collapse whitespace
+        .str.strip()                                                # trim whitespace
+    )
+    return df
 
 
-df1 = pd.read_excel("Aptamers1.xlsx") 
-df2 = pd.read_excel("Aptamers2.xlsx", skiprows = 1)
+
+df1 = pd.read_excel("Datasets/Aptamers1.xlsx") 
+df2 = pd.read_excel("Datasets/Aptamers2.xlsx", skiprows = 1)
+df1 = normalize_header(df1)
+
 combined = pd.concat([df1, df2], ignore_index = True)
-df_cleaned_cols = combined[["Type of Nucleic Acid","Target", "Aptamer Sequence","Sequence Length"]]
+
+
+df_cleanedcols = combined[["Type of Nucleic Acid","Target", "Aptamer Sequence","Sequence Length"]]
 df_nodupes = df_cleanedcols.drop_duplicates(subset = ['Target'])
 df_nodupes.to_excel("Cleaned_Aptamer_Data.xlsx", index=False)
 
@@ -23,12 +40,12 @@ def screen_prot(target_names):
     # protein_keywords = ['protein', 'enzyme', 'receptor', 'antibody', 'kinase', 'factor', 'ligase', 'polymerase']
     for target in target_names: 
         if any(keyword in target.lower() for keyword in non_protein_keywords):
-            mask.add(False)
+            mask.append(False)
         else:
-            mask.add(True)
+            mask.append(True)
     return mask
 
-target_col = df_nodupes["Target"].tolist()
+target_col = df_nodupes["Target"].fillna("").astype(str).tolist()
 mask = screen_prot(target_col)
 filtered = df_nodupes[mask] #removes screened proteins
 
@@ -37,29 +54,13 @@ keeplist = []
 
 for target in target_col:
     
-    with Entrez.esearch(db= "protein", term = target_name) as handle:
+    with Entrez.esearch(db= "protein", term = target) as handle:
         h = Entrez.read(handle)
     ids = h.get("IDList", [])
     keeplist.append[len(ids) > 0]
     
-df proteins = df_cleanedcols[keeplist].reset_index(drop = True)
-
-
-
-
-
-'''
-for target in targetcol:
-    with Entrez.esearch(db= "protein", term = target_name) as handle:
-        h = Entrez.read(handle)
-    ids = h.get("IDList", [])
-    top_id = ids[0]
-    with fasta = Entrez.efetch(ds = "protein", id = top_id, rettype = "fasta", retmode = "text") as f:
-        fastatext = f.read()
-'''
-
-
-
+df_proteins = df_cleanedcols[keeplist].reset_index(drop = True)
+print(keeplist)
     
         
         
